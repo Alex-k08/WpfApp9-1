370f9b52cb2f7b3d10142e77e251e2335da8226f
Nikita <angel311@bk.ru>
2022-06-07 13:01:08 +0800

    init

---
+++ WpfApp9/Page3.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Логика взаимодействия для Page3.xaml
+    /// </summary>
+    public partial class Page3 : Page
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        private void voity_Click(object sender, RoutedEventArgs e)
+        {
+            var db = new vlad_kEntities();
+            var Users = db.Users.Where ((user) => user.Login == Login.Text && user.Password == Password.Password).ToList();
+            
+        }
+    }
+}
